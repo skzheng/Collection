57c04d6ceb7a09b4977087bb1b2e9dee8a55fd4b
skzheng <simonkzheng@gmail.com>
2018-03-14 21:06:02 -0400

    Added cached function

--- nthFibonacci.js
+++ nthFibonacci.js
@@ -5,6 +5,28 @@ function nthFibonacci(n){
   return nthFibonacci(n-2) + nthFibonacci(n-1);
 }
 
+const nthFibonacciCached = ((n) => {
+  let cache = {};
+  return (n) => {
+    let res;
+    
+    // console.log(cache) // Checking cache items
+    
+    if(cache[n]) {
+      return cache[n]
+    } else {
+      if(n < 2) {
+        res = n;
+      }
+      else {
+        res = nthFibonacciCached(n- 1) + nthFibonacciCached(n-2);
+      }
+      cache[n] = res;
+      return res;
+    }
+  }
+})();
+
 nthFibonacci(3); // 2
 nthFibonacci(5); // 5
 
