9cd1316b45cc99d5100ea2340c76fa51bb6e45dd
skzheng <simonkzheng@gmail.com>
2018-03-17 14:32:58 -0400

    Create encodeDecode.js

---
+++ encodeDecode.js
@@ -0,0 +1,21 @@
+let store = {};
+
+function encode(url){
+  let timestamp = Date.now().toString(36);
+  store[timestamp] = url;
+  return `http://tinyurl.com/${timestamp}`
+}
+
+function decode(url){
+  let code = url.replace(/http:\/\/tinyurl.com\//, '');
+  return store[code] || "URL not found";
+}
+
+decode(encode('www.google.com'))
+// decode()
+
+/*
+TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk.
+
+Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+*/
